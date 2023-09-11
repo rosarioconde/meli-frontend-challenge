dc5412517c56db8363a314728675c36f153dda27
rosarioconde <conde.rosario88@gmail.com>
2023-09-11 18:03:02 -0300

    items - display list of products

---
+++ src/app/items/page.tsx
@@ -0,0 +1,42 @@
+
+
+export default async function ItemsPage({ searchParams}: { searchParams: { search: string }}) {
+  const { results } = await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${searchParams.search}`).then(res => res.json() as Promise<{
+    results: {
+      id: string;
+      title: string;
+      thumbnail: string;
+      price: number;
+      currency_id: string;
+      seller_address: {
+        city: {
+          name: string;
+      }
+    }
+    }[]
+   }>)
+
+
+  return (
+    <section>
+      <article className="grid gap-4">
+        {results.map((item) => (
+          <div key={item.id} className='flex gap-4'>
+              <img src={item.thumbnail} alt={item.title} />
+              <div>
+                <h1 className="text-xl font-bold">{item.title}</h1>
+                <h2>{Number(item.price).toLocaleString('es-AR',{
+                  style:'currency',
+                  currency:item.currency_id,
+                  })}</h2>
+                <h3>{item.currency_id}</h3>
+                <span className="ml-auto text-sm opacity-50 capitalize">
+                  {item.seller_address.city.name.toLowerCase()}
+                </span>
+              </div>
+          </div>
+        ))}
+      </article>
+    </section>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header className='h-16 bg-yellow-300 px-4 '>
-          <form action="/items" className='max-w-screen-xl m-auto'>
-            <input type="text" name="search" />
-            <button type='submit'>Buscar</button>
+        <header className='h-16 bg-yellow-300 px-4 flex'>
+          <form action="/items" className='flex max-w-screen-xl m-auto flex-1 gap-4'>
+            <input className='h-8 flex-1 text-gray-700 px-2' type="text" name="search" />
+            <button className='h-8 bg-gray-300 px-2 py-1 text-slate-700' type='submit'>Buscar</button>
           </form>
         </header>
         <main className='max-w-screen-xl p-4'>{children}</main>
